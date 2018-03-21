cd3b787991278bf88ba66851d6d46e28d8b00c37
widi9545 <widi9545@colorado.edu>
2018-03-21 13:15:47 -0600

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -92,10 +92,20 @@ function clickSelectedItem() {
 // this function responds to user key presses
 // you'll rewrite this to control your interface using some number of keys
 $(document).keypress(function(event) {
-	if (event.key == "a") {
-		alert("You pressed the 'a' key!")	
-	} else if (event.key == "b") {
-		alert("You pressed the 'b' key!")
+	var ItemToSelect = 0
+	if (event.key == "d") {
+			ItemToSelect = selectNext()
+	} else if (event.key == "a") {
+			ItemToSelect = selectPrevious()
+	}
+	else if (event.key == "w"){
+		ItemToSelect = selectUp()
+	}
+	else if (event.key == "s"){
+		ItemToSelect = selectDown()
+	}
+	else if (event.key == "c"){
+		ItemToSelect = clickSelectedItem()
 	}
 })
 
